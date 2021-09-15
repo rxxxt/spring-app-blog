958b3581bbfd0d24ca2e599c77a938024da5c6f0
tredfort <d.rinat703@gmail.com>
2021-09-15 22:10:35 +0300

    added the author field for the message

--- src/main/java/org/example/twitter/controller/MainController.java
+++ src/main/java/org/example/twitter/controller/MainController.java
@@ -1,31 +1,28 @@
-package org.example.twitter;
+package org.example.twitter.controller;
 
 import org.example.twitter.domain.Message;
+import org.example.twitter.domain.User;
 import org.example.twitter.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Map<String, Object> model
-    ) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
 
@@ -34,9 +31,13 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    @PostMapping("/main")
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model
+    ) {
+        Message message = new Message(text, tag, user);
 
         messageRepo.save(message);
 
@@ -61,6 +62,4 @@ public class GreetingController {
 
         return "main";
     }
-
-
 }

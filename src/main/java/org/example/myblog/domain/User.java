e02678e7bf32a0aa2b23e79711321540552cf172
tredfort <d.rinat703@gmail.com>
2021-11-07 21:56:20 +0300

    fixed a bug with getting an activation code

--- src/main/java/org/example/myblog/domain/User.java
+++ src/main/java/org/example/myblog/domain/User.java
@@ -19,9 +19,6 @@ public class User implements UserDetails {
     private String username;
     @NotBlank(message = "Password cannot ve empty")
     private String password;
-    @Transient
-    @NotBlank(message = "Password confirmation cannot ve empty")
-    private String password2;
     private boolean active;
 
     @Email(message = "Email is not correct")
@@ -101,14 +98,6 @@ public class User implements UserDetails {
 
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
-    }
-
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
     }
 
     public String getEmail() {
--- src/main/resources/templates/login.ftl
+++ src/main/resources/templates/login.ftl
@@ -7,6 +7,10 @@
             ${Session.SPRING_SECURITY_LAST_EXCEPTION.message}
         </div>
     </#if>
-    ${message?ifExists}
+    <#if message??>
+        <div class="aert alert-${messageType}" role="alert">
+            ${message}
+        </div>
+    </#if>
     <@l.login "/login" false/>
 </@c.page>
